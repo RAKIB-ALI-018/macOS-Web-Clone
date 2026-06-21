7b6d40c25cab5ac362b2449ef4b83b3e5f142e44
RAKIB-ALI-018 <rakibali99580@gmail.com>
2026-06-21 22:04:56 +0530

    Fix missing trailing slash in base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/macOS-Web-Clone",
+  base:"/macOS-Web-Clone/",
   define: {
     'process.env.NODE_ENV': JSON.stringify('development'),
   },
